0f39b906f6403372047aad61c9236dc810299f68
SOPHEAKTRA <96566898+yuuuuu-z@users.noreply.github.com>
2025-07-26 10:58:05 +0700

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,17 +1,25 @@
-CREATE TABLE USERS(
-  id uuid references auth.users not null primary key,
-  email text
+CREATE TABLE IF NOT EXISTS public.users (
+  id UUID PRIMARY KEY REFERENCES auth.users (id) ON DELETE CASCADE,
+  email TEXT,
+  full_name TEXT
 );
 
-create or replace function public.insert_user_on_sign_up()
-returns trigger as $$
-begin 
-  insert into public.users (id, email)
-  values (new.id, new.email);
-  return new;
-end;
-$$ language plpgsql security definer;
 
-create trigger on_new_user
-  after insert on auth.users
-  for each row execute procedure public.insert_user_on_sign_up();
+CREATE OR REPLACE FUNCTION public.insert_user_on_sign_up()
+RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO public.users (id, email, full_name)
+  VALUES (
+    NEW.id,
+    NEW.email,
+    NEW.raw_user_meta_data->>'full_name'
+  );
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+
+CREATE TRIGGER on_new_user
+AFTER INSERT ON auth.users
+FOR EACH ROW
+EXECUTE FUNCTION public.insert_user_on_sign_up();
